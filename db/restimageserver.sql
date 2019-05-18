d786fd76d396d06f079ac01a92311ddfc9451d01
Xerintel Proyectos <proyectos@xerintel.es>
2019-05-18 15:38:06 +0200

    db

--- db/restimageserver.sql
+++ db/restimageserver.sql
@@ -2,3 +2,24 @@
 -- Archivo de base de datos --
 ------------------------------
 
+DROP TABLE IF EXISTS grupos CASCADE;
+
+CREATE TABLE grupos
+(
+    id BIGSERIAL PRIMARY KEY
+);
+
+DROP TABLE IF EXISTS image CASCADE;
+
+CREATE TABLE image
+(
+    id         BIGSERIAL    PRIMARY KEY,
+    grupo_id   BIGINT       REFERENCES grupos(id)
+                            ON UPDATE CASCADE
+                            ON DELETE SET NULL,
+    nombre     VARCHAR(255) NOT NULL,
+    extension  VARCHAR(255) NOT NULL,
+    uri        VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP
+);
